4a08f8bd3d7822a87e4e99ce32d414a555990ed8
anreh <anurehka@gmail.com>
2022-08-02 10:42:48 +0800

    Commit 20220802 - Added baseline constraint - Added clickhandler

--- app/src/main/java/com/example/contrainlayout/MainActivity.kt
+++ app/src/main/java/com/example/contrainlayout/MainActivity.kt
@@ -19,10 +19,13 @@ class MainActivity : AppCompatActivity() {
         val boxThreeText = findViewById<TextView>(R.id.boxThree)
         val boxFourText = findViewById<TextView>(R.id.boxFour)
         val boxFiveText = findViewById<TextView>(R.id.boxFive)
+        val redButton = findViewById<Button>(R.id.red_button)
+        val greenButton = findViewById<Button>(R.id.green_button)
+        val yellowButton = findViewById<Button>(R.id.yellow_button)
 
         val clickableViews: List<View> =
             listOf(
-                boxOneText, boxTwoText, boxThreeText, boxFourText, boxFiveText
+                boxOneText, boxTwoText, boxThreeText, boxFourText, boxFiveText, redButton, greenButton, yellowButton
             )
 
         for(item in clickableViews){
@@ -37,6 +40,10 @@ class MainActivity : AppCompatActivity() {
             R.id.boxThree -> view.setBackgroundColor(Color.MAGENTA)
             R.id.boxFour -> view.setBackgroundColor(Color.YELLOW)
             R.id.boxFive -> view.setBackgroundColor(Color.CYAN)
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
+
             else -> view.setBackgroundColor(Color.GRAY)
         }
     }
